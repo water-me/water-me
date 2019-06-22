9fdd8ac9fd9048f070923221e558c9d81d75d51b
Joshua Owens <joshuaowens@protonmail.com>
2019-06-21 19:14:02 -0500

    Setting up basic routing

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,4 @@
 FROM php:7.3-apache
+
+RUN a2enmod rewrite
+RUN service apache2 restart
--- src/index.php
+++ src/index.php
@@ -1,3 +1,18 @@
 <?php
 
-echo 'Hello World';
+$request = $_SERVER['REDIRECT_URL'];
+
+switch ($request) {
+    case '/' :
+        // require __DIR__ . '/views/index.php';
+        echo 'Hello World';
+        break;
+    case '' :
+        // require __DIR__ . '/views/index.php';
+        echo 'Hello World';
+        break;
+    default:
+        // require __DIR__ . '/views/404.php';
+        echo 'Nope';
+        break;
+}
