b132b0895b11979830986d76327f8d04d7e5ad5f
Jenny Decker <jennydecker@Jennys-MacBook-Pro-2.local>
2019-07-20 12:38:08 -0500

    created list of plant names and types

--- src/web/plants.php
+++ src/web/plants.php
@@ -7,7 +7,20 @@
   <title>My Plants</title>
 </head>
 <body>
-  <!-- Jenny make list of our plants here - make it an unodered list -->
-  <!-- In this format: PLANT NAME, PLANT TYPE -->
+  <ul>
+    <li>George, Chamaedorea Elegans</li>
+    <li>Peter, Chlorophytum comosum</li>
+    <li>Bill, Pachira aquatica</li> 
+    <li>Small Peace Lily, Spathiphyllum wallisii</li>
+    <li>Big Green, Questionable Plant Name?</li>
+    <li>Snape, Sansevieria trifasciata</li>
+    <li>Nagini, Sansevieria trifasciata</li>
+    <li>Francis, Mystery Tree thing</li>
+    <li>Brienne of Tarth, Lil mix of green plants</li>
+    <li>Dino, Haworthia Succulent</li>
+    <li>Dart, Succulent</li>
+    <li>Bernie, Aloe</li>
+    <li>Jo, Epipremnum aureum</li>
+  </ul>
 </body>
 </html>
